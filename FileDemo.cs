af7278016273a8da646a9111868e24f67546fd9c
John Rowley <outsidethebox@eircom.net>
2021-11-02 15:55:35 +0000

    Add project files.

---
+++ FileDemo.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile002
+{
+    public class FileDemo
+    {
+
+        public static void ReadDemo(string file = "jamaica.txt")
+        {
+            string path = "c:/users/outsi/downloads";
+
+
+
+            string fileToRead = $"{path}/{file}";
+
+            Console.WriteLine($"Reading File: {fileToRead}");
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileToRead))
+                {
+
+                    Console.WriteLine($"Starting to read {fileToRead}");
+
+                    string line;
+
+                    while ((line = sr.ReadLine()) is not null)
+                    {
+
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"The {fileToRead} file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+
+        public static void WriteDemo(string file, string[] heroes)
+        {
+
+            string path = "c:/users/outsi/downloads";
+           
+            string fileToWrite = $"{path}/{file}";
+       
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileToWrite))
+                {
+                    foreach (string hero in heroes)
+                    {
+
+                        sw.WriteLine(hero);
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"The {fileToWrite} file could not be written");
+                Console.WriteLine(e.Message);
+
+            }
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFile002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileDemo.ReadDemo("jamaica.txt");
+
+            string dc_heroesFile = "heroes-dc.txt";
+            string[] dc_heroes = { "Batman:Bruce Wayne", "Superman:Clarke Kent", "Aquaman:Arthur Curry" };
+
+            FileDemo.WriteDemo(dc_heroesFile, dc_heroes);
+
+
+
+            string marvel_heroesFile = "heroes-marvel.txt";
+            string[] marvel_heroes = { "SpiderMan:Peter Parker", "The Hulk:David Banner", "Captain America:Steve Rogers" };
+            FileDemo.WriteDemo(marvel_heroesFile, marvel_heroes);
+
+
+
+
+            Console.WriteLine("Done");
+
+
+        }
+    }
+}
